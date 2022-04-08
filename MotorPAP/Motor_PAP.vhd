cc5892ac592d11d0b879d787fc0f9d407ab83230
Albert Morales <albertone0127@gmail.com>
2022-04-07 22:25:40 -0500

    Modifique el tipo de paso de simple a paso doble en las salidas a los puertos de la linea 36

--- MotorPAP/Motor_PAP.vhd
+++ MotorPAP/Motor_PAP.vhd
@@ -12,7 +12,7 @@ END MotorPAP;
 
 ARCHITECTURE motor OF MotorPAP IS
 
-signal conta_1250us: integer range 1 to 800000:=1; -- pulso1 de 1250us@400Hz (0.25ms) 62500
+signal conta_1250us: integer range 1 to 62500:=1; -- pulso1 de 1250us@400Hz (0.25ms) 62500
 signal sw: std_logic_vector(1 downto 0);
 signal SAL_400Hz: STD_LOGIC; -- reloj de 400Hz
 
@@ -21,7 +21,7 @@ process (CLK,SAL_400Hz) begin
 
 
 if rising_edge(CLK) then
-	if (conta_1250us = 800000) then --cuenta 1250us (50MHz=62500)
+	if (conta_1250us = 62500) then --cuenta 1250us (50MHz=62500)
 			-- if (conta_1250us = 125000) then --cuenta 1250us (100MHz=125000)
 			SAL_400Hz <= NOT(SAL_400Hz); --genera un barrido de 2.5ms
 			conta_1250us <= 1;
@@ -33,10 +33,10 @@ end if;
 
 IF SAL_400Hz'EVENT and SAL_400Hz='1' THEN sw <= sw + '1';
 	case sw is
-	when "00" => leds <= "1000"; puerto <= "1000";
-	when "01" => leds <= "0100"; puerto <= "0100";
-	when "10" => leds <= "0010"; puerto <= "0010";
-	when others => leds <= "0001"; puerto <= "0001";
+	when "00" => leds <= "1100"; puerto <= "1100";
+	when "01" => leds <= "0110"; puerto <= "0110";
+	when "10" => leds <= "0011"; puerto <= "0011";
+	when others => leds <= "1001"; puerto <= "1001";
 	end case;
 end if;
 
