8e58aa2267a4f41a6aad59fe07cf48c5450d47b5
Alberthacos <albertone0127@gmail.com>
2022-04-11 13:56:04 -0500

    Funciona perfecto solo tiene comentarios extra

--- motor_encoder/encoder.vhd
+++ motor_encoder/encoder.vhd
@@ -31,10 +31,11 @@ architecture Behavioral of Encoder is
 type stateType is ( idle, R1, R2, R3, L1, L2, L3, add, sub);
 signal curState, nextState: stateType;
 signal conter: integer range 1 to 3;
-signal ciclo: integer range 0 to 25:=0;
-signal conta_1250us: integer range 1 to 62000:=1; -- pulso1 de 1250us@400Hz (0.25ms) 62500
+signal ciclo1,ciclo2: integer range 0 to 10:=0;
+signal conta_1250us: integer range 1 to 60000:=1; -- pulso1 de 1250us@400Hz (0.25ms) 62500
 signal SAL_400Hz: STD_LOGIC; -- reloj de 400Hz
-signal sw: std_logic_vector(3 downto 0):="0000";
+signal sw1,sw2: std_logic_vector(3 downto 0):="0000";
+signal sd1,sd2: std_logic:='0';
 begin
 
 --clk and button
@@ -52,24 +53,28 @@ begin
                 if (curState = add) then
                     if EncOut < "10011" then
                         EncOut <= EncOut+1;
-                        conter <=1;
+                     --  conter <=1;
                     else
                         EncOut <= "00000";
+                    --    conter <=3;
+                     --   conter <=1;
                       
                     end if;
 
                 elsif (curState = sub) then
                     if EncOut > "00000" then
                         EncOut <= EncOut-1;
-                        conter <=2;
+                     --  conter <=2;
                     else
                         EncOut <= "10011";
+                 --       conter <=3;                      
+                        -- conter <=2;
                        
                     end if;
 
                 else
                     EncOut <= EncOut;
-                    conter <= 3;
+                  --  conter <= 3;
                    
                 end if;
 
@@ -85,20 +90,20 @@ next_state: process (curState, A, B)
 begin
 case curState is
 
-    --detent position
-    when idle => 
+    --detent position 
+    when idle =>  conter<=3;
         LED<= "00"; 
         if B = '0' then
-            nextState <= R1;
+            nextState <= R1; conter<=1;
         elsif A = '0' then
-            nextState <= L1;
+            nextState <= L1; conter<=2;
         else
              nextState <= idle;
         end if;
 
     -- start of right cycle
     --R1
-    when R1 =>
+    when R1 => conter<=3; conter<=1;
         LED<= "01";
         if B='1' then
             nextState <= idle;
@@ -109,8 +114,7 @@ case curState is
         end if;
 
     --R2
-    when R2 =>
-   -- conter <='1';
+    when R2 =>-- conter<=3; conter <=1;
         LED<= "01";
         if A ='1' then
             nextState <= R1;
@@ -121,7 +125,7 @@ case curState is
         end if;
 
     --R3
-    when R3 =>
+    when R3 => conter<=3; conter <=1;
             LED<= "01";
         if B ='0' then
             nextState <= R2;
@@ -131,13 +135,13 @@ case curState is
             nextState <= R3;
         end if;
 
-    when add =>
+    when add => conter <=3; conter <=1;
         LED<= "01";
         nextState <= idle;
 
         -- start of left cycle
         --L1
-    when L1 =>
+    when L1 => conter<=3; conter <=2;
             LED<= "10";
         if A ='1' then
             nextState <= idle;
@@ -148,7 +152,7 @@ case curState is
         end if;
     
         --L2
-    when L2 =>
+    when L2 => conter<=3; conter <=2;
             LED<= "10";
         if B ='1' then
             nextState <= L1;
@@ -159,7 +163,7 @@ case curState is
         end if;
     
          --L3
-    when L3 =>
+    when L3 => conter<=3; conter <=2;
         LED<= "10";
         if A ='0' then
             nextState <= L2;
@@ -169,13 +173,13 @@ case curState is
             nextState <= L3;
         end if;
 
-    when sub =>
+    when sub => conter <=3; conter <=2;
         LED<= "10";
         nextState <= idle;
 
     when others =>
         LED<= "11";
-        nextState <= idle;
+        nextState <= idle; 
 end case;
 end process;
 
@@ -191,28 +195,35 @@ puerto: process(CLK,conter) begin
             end if;
         end if;
 
-    if conter = 1 then 
-        IF SAL_400Hz'EVENT and SAL_400Hz='1' and sw /= "0101" and ciclo /=17 THEN sw <= sw + '1';
-			  case sw is
-			  when "0000" => leds <= "1100"; puertos <= "1100";
-			  when "0001" => leds <= "0110"; puertos <= "0110";
-			  when "0010" => leds <= "0011"; puertos <= "0011";
-			  when "0011" => leds <= "1001"; puertos <= "1001";
-			  when others => leds <= "0000"; puertos <= "0000"; ciclo <= ciclo+1; sw <="0000";
-			  end case;
+    
+ 
+IF SAL_400Hz'EVENT and SAL_400Hz='1'  then
+    if (conter = 1 or sd1= '1') and sd2 ='0' then
+        if sw1 /= "0100" and ciclo1 /=10 THEN sw1 <= sw1 + '1';
+                case sw1 is
+                when "0000" => leds <= "1100"; puertos <= "1100"; sd1 <='1';
+                when "0001" => leds <= "0110"; puertos <= "0110"; sd1 <='1';
+                when "0010" => leds <= "0011"; puertos <= "0011"; sd1 <='1';
+                when others => leds <= "1001"; puertos <= "1001"; sd1 <='1'; ciclo1 <= ciclo1+1; sw1 <="0000"; --leds <= "0000"; puertos <= "0000"; 
+                end case;
+               
         end if;
-    elsif conter = 2 then 
-        IF SAL_400Hz'EVENT and SAL_400Hz='1' and sw /= "0101" and ciclo /=17  THEN sw <= sw + '1';
-            case sw is
-            when "0000" => leds <= "1001"; puertos <= "1001";--1001
-            when "0001" => leds <= "0011"; puertos <= "0011";--0011
-            when "0010" => leds <= "0110"; puertos <= "0110";--0110
-            when "0011" => leds <= "1100"; puertos <= "1100";--0110
-            when others => leds <= "0000"; puertos <= "0000"; ciclo <= ciclo+1; sw <= "0000";--1100 
-            end case;
+
+    elsif (conter = 2 or sd2 ='1') and sd1 ='0'then 
+        if sw2 /= "0100" and ciclo2 /=10  THEN sw2 <= sw2 + '1';
+                case sw2 is
+                when "0000" => leds <= "1001"; puertos <= "1001"; sd2 <='1';--1001
+                when "0001" => leds <= "0011"; puertos <= "0011"; sd2 <='1';--0011
+                when "0010" => leds <= "0110"; puertos <= "0110"; sd2 <='1';--0110
+                when others => leds <= "1100"; puertos <= "1100"; sd2 <='1'; ciclo2 <= ciclo2+1; sw2 <= "0000"; sd2 <='1'; --leds <= "0000"; puertos <= "0000"; --1100 
+                end case;
+                
         end if;
-     else sw <="0000"; ciclo <=0; leds<="0000"; puertos<="0000";
+    else sw1 <="0000"; ciclo1 <=0; sw2 <="0000"; ciclo2 <=0;-- leds<="0000"; puertos<="0000";
     end if;
+    if (ciclo1 = 10 or ciclo2 = 10) then leds <= "0000"; puertos <= "0000"; sd1 <='0'; sd2 <='0'; end if;
+    
+end if;
 
     end process;
 
